b57eacf89151fe701ac28684b9b59484debaa8c0
dlon450 <dlon450@aucklanduni.ac.nz>
2022-11-12 21:48:30 -0500

    added saving and fixed constraints

--- src/main.py
+++ src/main.py
@@ -1,16 +1,9 @@
 import pandas as pd
 import numpy as np
-
 from model import solve_lp_prime, solve_lp_alpha, topsis
 from prepare_data import get_social_utilities
 
-ALPHA = np.arange(0., 1.1, 1.)
-TOTAL = 1e8 # initial investment
-TIMEFRAME = 5 # years
-INVESTMENT = 6500 # per family
-GROWTH_RATE = 0.06
-
-def optimize(fn):
+def optimize(fn, needs_colnames, initial, alphas, investment, timeframe, discount, interest, repayment):
     '''
     Formulate LP' and LP to find optimal strategy for investment allocation.
 
@@ -28,42 +21,46 @@ def optimize(fn):
     x : (J, N) binary np.array
         Optimal solution/strategy for investment allocation.
     '''
-    communities_df = pd.read_csv(fn).dropna().sample(frac=0.001, random_state=2)
+    communities_df = pd.read_csv(fn).dropna()#.sample(frac=0.001, random_state=2)
 
+    # filter out households that cannot meet monthly requirement
+    communities_df = communities_df[communities_df["Average monthly Income per Household (USD/m)"] >= 3 * repayment]
     J = len(communities_df)
 
-    ####### filter out households that cannot meet monthly requirement ##########
-
-    needs_colname = [
-        "% of Population without Adequate Housing",
-        "% of Population without Electricity",
-        "% of Population without Water and Sanitation Services",
-        "% of Households without access to Internet (Based on people without refrigerator)",
-        "% of People without acces to quality Medical services (Municipality)",
-    ]
-
-    proportions_communities = communities_df[needs_colname].to_numpy()
+    proportions_communities = communities_df[needs_colnames].to_numpy()
     households_communities = communities_df["Number of Households"].to_numpy()
-    monthly_household_incomes = communities_df["Average monthly Income per Household (USD/m)"].to_numpy()
 
     social_utilities = get_social_utilities(communities_df, proportions_communities, households_communities)
-    cash_flows = monthly_household_incomes * households_communities * 12 # yearly cash flow
+    cash_flows = np.array([repayment for _ in range(J)])
 
-    S_prime, N_ = solve_lp_prime(J=J, N=TIMEFRAME, S=social_utilities, C=INVESTMENT*households_communities, \
-        r=GROWTH_RATE, M=TOTAL, a=cash_flows)
+    S_prime, N_, covering = solve_lp_prime(J=J, N=timeframe, S=social_utilities, C=investment*households_communities, \
+        r1=discount, r2=interest, M=initial, a=cash_flows)
     
-    N_ = TIMEFRAME ###### for now #######
+    N_ = timeframe ###### for now #######
 
-    n = len(ALPHA)
+    n = len(alphas)
     npv_all, s_all, x_all = np.zeros(n), np.zeros(n), np.zeros((n, J, N_))
-    for i, alpha in enumerate(ALPHA):
-        npv_all[i], s_all[i], x_all[i] = solve_lp_alpha(J, N=N_, S=social_utilities, C=INVESTMENT*households_communities, r=GROWTH_RATE, M=TOTAL, \
-            a=cash_flows, S_prime=S_prime, alpha=alpha)
+    for i, alpha in enumerate(alphas):
+        npv_all[i], s_all[i], x_all[i] = solve_lp_alpha(J, N=N_, S=social_utilities, C=timeframe*households_communities, \
+            r1=discount, r2=interest, M=initial, a=cash_flows, S_prime=S_prime, covering=covering, alpha=alpha, save=True)
 
-    i_ = topsis(npv_all, s_all)
-
-    return npv_all[i_], s_all[i_], x_all[i_]
+    return npv_all, s_all, x_all
 
 if __name__ == "__main__":
-    fn = "data/2020_communities.csv"
-    optimize(fn)
+
+    fn = "example/2020_communities.csv"
+    needs = ["% of Population without Adequate Housing", "% of Population without Electricity", "% of Population without Water and Sanitation Services", "% of Households without access to Internet (Based on people without refrigerator)", "% of People without acces to quality Medical services (Municipality)"]
+    
+    alphas = np.arange(0., 1.1, 1.)
+    total_initial_investment = 1e8
+    timeframe = 5 # years
+    timeframe_stepsize_in_months = 12
+    investment_per_family = 6500
+    discount_rate = 0.06
+    interest_rate = 0.06
+    min_repayment_per_timestep = investment_per_family / timeframe / timeframe_stepsize_in_months
+
+    npv, s, x = optimize(fn, needs, alphas, total_initial_investment, timeframe, timeframe_stepsize_in_months, \
+        investment_per_family, discount_rate, interest_rate, min_repayment_per_timestep)
+    
+    print(npv, s)
--- src/model.py
+++ src/model.py
@@ -1,9 +1,10 @@
 import gurobipy as gp
 from gurobipy import GRB 
+import os
 import time
 import numpy as np
 
-def solve_lp_prime(J, N, S, C, r, M, a):
+def solve_lp_prime(J, N, S, C, rd, ri, M, a, save=True):
     '''
     Solve LP' and return S', x', N
 
@@ -17,12 +18,16 @@ def solve_lp_prime(J, N, S, C, r, M, a):
         Social utilities for each community j.
     C : (J, 1) np.array of floats
         Cost of investment for all households in community j.
-    r : float
+    rd : float
         Capital growth rate (i.e., discount rate).
+    ri : float
+        Interest rate for cash flows.
     M : float
         Total initial investment for month 1.
     a : (J, 1) np.array of floats
         Cashflows per timestep for each community j.
+    save : bool
+        Indicate to save solution in 'results' directory.
 
     Returns
     -------
@@ -30,6 +35,8 @@ def solve_lp_prime(J, N, S, C, r, M, a):
         Optimal objective (social utility) for LP'.
     N_prime : int
         First timestep where all communities have been reached.
+    covering : bool
+        Indicate if solution covers all communities.
     '''
     st = time.time()
 
@@ -38,9 +45,9 @@ def solve_lp_prime(J, N, S, C, r, M, a):
     z = model.addVars(J, N, vtype=GRB.BINARY, name='z')
 
     print('Setting objective...')
-    model.setObjective(total_social_utility(S, x, r, N), GRB.MAXIMIZE)
+    model.setObjective(total_social_utility(S, x, rd, N), GRB.MAXIMIZE)
 
-    set_budget(model, x, z, C, M, N, r, a)
+    set_budget(model, x, z, C, M, N, ri, a)
     set_stationarity(model, J, x, z, N)
     
     ct = time.time()
@@ -49,11 +56,16 @@ def solve_lp_prime(J, N, S, C, r, M, a):
     S_prime, x_prime = solve(model, J, N)
     print("Elapsed Time:", time.time() - ct, "seconds")
 
-    N_prime = get_first_covering_timestep(x_prime)
+    covering, N_prime = get_first_covering_timestep(x_prime)
 
-    return S_prime, N_prime
+    if save:
+        if not os.path.exists('results'):
+            os.makedirs('results')
+        np.savetxt(f'results/x_prime_{S_prime:.2f}.csv', x_prime.astype(int), delimiter=',', header=f'x_prime with objective {S_prime}')
 
-def solve_lp_alpha(J, N, S, C, r, M, a, S_prime, alpha):
+    return S_prime, N_prime, covering
+
+def solve_lp_alpha(J, N, S, C, rd, ri, M, a, S_prime, covering, alpha, save=True):
     '''
     Solve LP and return objective and solution
 
@@ -67,14 +79,18 @@ def solve_lp_alpha(J, N, S, C, r, M, a, S_prime, alpha):
         Social utilities for each community j.
     C : (J, 1) np.array of floats
         Cost of investment for all households in community j.
-    r : float
+    rd : float
         Capital growth rate (i.e., discount rate).
+    ri : float
+        Interest rate for cash flows.
     M : float
         Total initial investment for month 1.
     a : (J, 1) np.array of floats
         Cashflows per timestep for each community j.
     S_prime : float
         Optimal objective (social utility) for LP'
+    covering : bool
+        Indicate if solution covers all communities.
     alpha : float
         Minimum proportion of S_prime to be achieved in LP
 
@@ -94,23 +110,30 @@ def solve_lp_alpha(J, N, S, C, r, M, a, S_prime, alpha):
     z = model.addVars(J, N, vtype=GRB.BINARY, name='z')
 
     print("Setting Objective...")
-    model.setObjective(gp.quicksum( a @ x.select('*',n) / (1+r)**n for n in range(N) ), GRB.MAXIMIZE)
+    model.setObjective(gp.quicksum( a @ x.select('*',n) / (1+rd)**n for n in range(N) ), GRB.MAXIMIZE)
 
-    set_budget(model, x, z, C, M, N, r, a)
+    set_budget(model, x, z, C, M, N, ri, a)
     set_stationarity(model, J, x, z, N)
 
     print("\nSetting social utility constraint...")
-    model.addConstr(total_social_utility(S, x, r, N) >= alpha*S_prime)
+    model.addConstr(total_social_utility(S, x, rd, N) >= alpha*S_prime)
 
-    print("Setting covering constraint...")
-    model.addConstr(z.sum('*', '*') == J)
+    if covering:
+        print("Setting covering constraint...")
+        model.addConstr(z.sum('*', '*') == J)
 
     ct = time.time()
     print("Elapsed time:", ct - st, "seconds")
     print('Solving...')
     npv, x_best = solve(model, J, N)
     print("Elapsed Time:", time.time() - ct, "seconds")
-    s_best = sum([S.dot(x_best[:,i]) / (1+r)**i for i in range(N)])
+    s_best = sum([S.dot(x_best[:,i]) / (1+rd)**i for i in range(N)])
+
+    if save:
+        if not os.path.exists('results'):
+            os.makedirs('results')
+        np.savetxt(f'results/x{alpha}_{npv:.2f}.csv', x_best.astype(int), delimiter=',', \
+            header=f'x with alpha={alpha} and objective={npv} and s={s_best}')
 
     return npv, s_best, x_best
 
@@ -139,20 +162,14 @@ def total_social_utility(S, x, r, N):
     return gp.quicksum( S @ x.select('*',n) / (1+r)**n for n in range(N) )
 
 def solve(m, J, N):
-    m.optimize()
-
-    # for v in m.getVars():
-        # print('%s %g' % (v.varName, v.x))
-    
+    m.optimize()    
     print('Obj: %g' % m.objVal)
-
     x_ = m.getAttr("X", m.getVars()[:J*N])
     x_ = np.array(x_).reshape((J, N))
-
-    # z_ = m.getAttr("X", m.getVars()[J*N:])
-    # z_ = np.array(z_).reshape((J, N))
-
     return m.objVal, x_
 
 def get_first_covering_timestep(x_):
-    return np.min(np.where(x_.all(axis=0))[0])
+    try:
+        return True, np.min(np.where(x_.all(axis=0))[0])
+    except ValueError:
+        return False, x_.shape[1]
